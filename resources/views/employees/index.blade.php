9bbb578c275d15ef2888d850c35c72878e2d9767
dindaauliaan <dindaauliaan2727@gmail.com>
2025-10-03 15:00:20 +0700

    penambahan master layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-</head>
-<body>
-    <div class="container mt-5">
-        <h1 class="mb-4">Daftar Pegawai</h1>
-        <table border="1" cellpadding="5" cellspacing="0">
-            <thead>
-                <tr>
+@extends('master')
+@section('title', 'Daftar Pegawai')
+@section('content')
+<div class="container mt-5">
+    <h1 class="mb-4">Daftar Pegawai</h1>
+    <table border="1" cellpadding="5" cellspacing="0">
+        <thead>
+            <tr>
                 <th>Nama Lengkap</th>
                 <th>Email</th>
                 <th>Nomor Telepon</th>
@@ -16,11 +14,11 @@
                 <th>Tanggal Masuk</th>
                 <th>Status</th>
                 <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($employees as $employee)
-                <tr>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($employees as $employee)
+            <tr>
                 <td>{{ $employee->nama_lengkap }}</td>
                 <td>{{ $employee->email }}</td>
                 <td>{{ $employee->nomor_telepon }}</td>
@@ -29,18 +27,17 @@
                 <td>{{ $employee->tanggal_masuk }}</td>
                 <td>{{ $employee->status }}</td>
                 <td>
-                <a href="{{ route('employees.show', $employee->id) }}">Detail</a> |
-                <a href="{{ route('employees.edit', $employee->id) }}">Edit</a> |
-                <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Delete</button>
-                </form>
+                    <a href="{{ route('employees.show', $employee->id) }}">Detail</a> |
+                    <a href="{{ route('employees.edit', $employee->id) }}">Edit</a> |
+                    <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" onclick="return confirm('Yakin ingin menghapus?')">Delete</button>
+                    </form>
                 </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        </div>
-    </body>
-</html>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
